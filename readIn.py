74be942da71390b95f0ccba819e305c0245242ec
Gergo Gyori <gergo.gyori@gmail.com>
2023-05-02 14:46:43 +0200

    t

--- readIn.py
+++ readIn.py
@@ -1,13 +1,45 @@
+def critical_path(execution_times, earliest_start_times, graphConnections):
+    """# Step 1: Create a dictionary to store execution times
+    execution_times = {node: dag.nodes[node]['execution_time'] for node in dag}
+
+    # Step 2: Create a dictionary to store earliest start times
+    earliest_start_times = {node: -float('inf') for node in dag}
+    earliest_start_times[dag.source()] = 0
+
+    # Step 3: Topological sort
+    sorted_nodes = list(nx.topological_sort(dag))
+    """
+
+    # Step 4: Calculate earliest start times
+    for node in sorted_nodes:
+        for predecessor in dag.predecessors(node):
+            earliest_start_times[node] = max(
+                earliest_start_times[node], earliest_start_times[predecessor] + execution_times[predecessor])
+
+    """# Step 5: Calculate latest completion times
+    latest_completion_times = {node: earliest_start_times[dag.sink()] for node in dag}
+    for node in reversed(sorted_nodes):
+        for successor in dag.successors(node):
+            latest_completion_times[node] = min(
+                latest_completion_times[node], latest_completion_times[successor] - execution_times[node])
+    """
+    # Step 6: Calculate critical path duration
+    critical_path_duration = latest_completion_times[dag.source()] - earliest_start_times[dag.source()]
+
+    return critical_path_duration
+
 n = int(input())
 # node time dictionary 
-taskTimeDict = {}
+execution_times = {}
+earliest_start_times = {}
 # graph dictionary
 graph = {}
 for i in range(n):
     line = input().split()
     node, time = line[0], line[1]
     # append weight of the node
-    taskTimeDict[node] = time
+    execution_times[node] = time
+    earliest_start_times[node] = -float('inf')
     graph[node] = list()
     if len(line) < 3:
         # do nothing
@@ -29,6 +61,6 @@ for key, value in graph.items():
     for v in value:
         graphConnections.append([int(v), int(key)])
 print(graphConnections)
-print(taskTimeDict)
+print(execution_times)
 # print(critical_path(graphConnections))
 #print(nodeTimeDict)
---
+++ topologicalShort.py
@@ -0,0 +1,46 @@
+def topological_sort(graph):
+    """
+    Perform a topological sort on a directed acyclic graph (DAG).
+
+    Args:
+        graph: A dictionary representing a DAG, where each key is a node and
+               the corresponding value is a list of its adjacent nodes.
+
+    Returns:
+        A list of nodes in topological order.
+
+    Raises:
+        ValueError: If the graph contains a cycle.
+    """
+    # Create a dictionary to keep track of the in-degree of each node
+    in_degree = {node: 0 for node in graph}
+
+    # Calculate the in-degree of each node
+    for node in graph:
+        for adj_node in graph[node]:
+            in_degree[adj_node] += 1
+
+    # Create a queue to store nodes with zero in-degree
+    zero_in_degree = [node for node in in_degree if in_degree[node] == 0]
+
+    # Create a list to store the topological order of nodes
+    topo_order = []
+
+    # Process each node with zero in-degree
+    while zero_in_degree:
+        node = zero_in_degree.pop(0)
+        topo_order.append(node)
+
+        # Decrease the in-degree of each adjacent node
+        for adj_node in graph[node]:
+            in_degree[adj_node] -= 1
+
+            # Add the adjacent node to the queue if its in-degree becomes zero
+            if in_degree[adj_node] == 0:
+                zero_in_degree.append(adj_node)
+
+    # Check if there is a cycle in the graph
+    if len(topo_order) != len(graph):
+        raise ValueError("The graph contains a cycle")
+
+    return topo_order
