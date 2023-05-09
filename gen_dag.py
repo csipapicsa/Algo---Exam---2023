cd1bcece963b851546e1787978af51aae75d6cb1
Markus Kristoffer Sibbesen <mksi@itu.dk>
2023-05-09 12:52:30 +0200

    changes

--- gen_dag.py
+++ gen_dag.py
@@ -1,4 +1,5 @@
 import random
+from collections import defaultdict
 
 n = 10
 max_time = 100
@@ -15,5 +16,20 @@ for ind, node in enumerate(nodes[1:]):
     depends = random.sample(graph.keys(), amount_depend)
     graph[node] = (random.randint(1, max_time), depends)
 
+op_graph = dict()
+for i in graph:
+    op_graph[i] = (graph[i][0], [])
+
+
+for i in graph:
+    for j in graph[i][1]:
+        print(i, j)
+        op_graph[j][1].append(i)
+
 for i in graph:
     print(f'{i}: {graph[i]}')
+
+print('------------------------')
+
+for i in op_graph:
+    print(f'{i}: {op_graph[i]}')
